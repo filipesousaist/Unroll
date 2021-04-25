5b760ee57464105bf1fa1cba5f510a14368e8854
pedro19v <ist190762@tecnico.ulisboa.pt>
2021-04-25 19:30:02 +0100

    Boy can now grab and do a strong push

--- Unroll/Assets/Scripts/Boy.cs
+++ Unroll/Assets/Scripts/Boy.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Boy : MonoBehaviour
 {
     private Rigidbody myRigidbody;
-    public float SPEED;
-    public float ANGULAR_SPEED;
+    public float PUSH_FORCE;
     public float DISTANCE;
     
     [SerializeField]
@@ -18,40 +17,49 @@ public class Boy : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if (Ball != null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Ball = null;
+
+            }
+
+            else if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Ball.GetComponent<Rigidbody>().AddForce(transform.forward * PUSH_FORCE);
+                Ball = null;
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Vector3 vectorStart = transform.position + new Vector3(0, 0.5f, 0);
+
+            if ((Physics.Raycast(vectorStart, transform.forward, out hit, 1.5f) ||
+                Physics.Raycast(vectorStart, Quaternion.AngleAxis(15, Vector3.up) * transform.forward, out hit, 1.5f) ||
+                Physics.Raycast(vectorStart, Quaternion.AngleAxis(-15, Vector3.up) * transform.forward, out hit, 1.5f))
+                && hit.transform.CompareTag("Ball"))
+            {
+                Ball = hit.transform;
+            }
+        }
+
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        myRigidbody.velocity = Vector3.zero;
-        myRigidbody.angularVelocity = Vector3.zero;
-
-        float rotY = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
-
-        if (Input.GetKey(KeyCode.W))
-            myRigidbody.velocity += new Vector3(SPEED * Mathf.Cos(rotY), 0, -SPEED * Mathf.Sin(rotY));
-        if (Input.GetKey(KeyCode.S))
-            myRigidbody.velocity += new Vector3(-SPEED * Mathf.Cos(rotY), 0, SPEED * Mathf.Sin(rotY));
-        if (Input.GetKey(KeyCode.A))
-            myRigidbody.angularVelocity += new Vector3(0, -ANGULAR_SPEED, 0);
-        if (Input.GetKey(KeyCode.D))
-            myRigidbody.angularVelocity += new Vector3(0, ANGULAR_SPEED, 0);
-
         if (Ball != null)
-        {
             BallPositionUpdate();
-        }
     }
 
     void BallPositionUpdate()
     {
-        Ball.position = new Vector3(transform.position.x + transform.right.x * DISTANCE, Ball.position.y, transform.position.z + transform.right.z * DISTANCE);
+        Ball.position = new Vector3(transform.position.x + transform.forward.x * DISTANCE, Ball.position.y, transform.position.z + transform.forward.z * DISTANCE);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.tag.Equals("Ball"))
-        {
-            //if (Input.GetKey(KeyCode.LeftShift))
-                Ball = collision.collider.transform;
-        }
-    }
 }
