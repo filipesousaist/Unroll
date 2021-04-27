23d89530f90bfdcaa8f9fee62243e62bdb27de69
ist190714 <ist190714@tecnico.ulisboa.pt>
2021-04-27 17:21:48 +0100

    Changes during office hours

--- Unroll/Assets/Scripts/Block.cs
+++ Unroll/Assets/Scripts/Block.cs
@@ -10,24 +10,37 @@ public class Block : MonoBehaviour
 
     private BlocksManager blocksManager;
 
+    private Ball ball;
+
+    private Rigidbody ballRigidbody;
+
+    private readonly float BALL_SPEED_DECREASE = 10;
+
     private void Start()
     {
         myCollider = GetComponent<Collider>();
         blocksManager = FindObjectOfType<BlocksManager>();
+        ball = FindObjectOfType<Ball>();
+        ballRigidbody = ball.GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Ball ball = other.gameObject.GetComponent<Ball>();
-        if (ball != null && ball.color.Equals(color))
+        if (other.gameObject.CompareTag("Ball") && ball.color.Equals(color))
         {
             blocksManager.RemoveBlock(this);
             Destroy(gameObject);
+
+            if (ballRigidbody.velocity.magnitude < BALL_SPEED_DECREASE)
+                ballRigidbody.velocity = Vector3.zero;
+            else
+                ballRigidbody.velocity -= ballRigidbody.velocity.normalized * BALL_SPEED_DECREASE;
         }    
     }
 
     public void SetBreakable(bool breakable)
     {
         myCollider.isTrigger = breakable;
+
     }
 }
