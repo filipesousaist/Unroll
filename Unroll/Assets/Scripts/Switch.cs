da5ff320090d58275ab5d916b47266c3b72cf0d2
j-galveias <brainsuprajr@gmail.com>
2021-04-29 22:38:42 +0100

    Maze button, key, lock and new jump value

---
+++ Unroll/Assets/Scripts/Switch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public MeshCollider col;
+    public MeshRenderer myRenderer;
+    public Material material;
+    public Transform mazeDoor;
+    private float translationConst = 1.42f;
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Ball")) {
+            myRenderer.material = material;
+            mazeDoor.position = new Vector3(mazeDoor.position.x - translationConst, mazeDoor.position.y, mazeDoor.position.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+}
