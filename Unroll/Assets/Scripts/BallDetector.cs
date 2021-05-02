948034e289656af394bdd5d6475b8c9b6e41df2d
j-galveias <brainsuprajr@gmail.com>
2021-05-02 02:00:33 +0100

    mind control room(Half working)

---
+++ Unroll/Assets/Scripts/BallDetector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDetector : MonoBehaviour
+{
+    public Helmet helmet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name.Equals("Ball"))
+        {
+            helmet.activate();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unroll/Assets/Scripts/Helmet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helmet : MonoBehaviour
+{
+    public MeshRenderer myRenderer;
+    public Material greenMaterial;
+    public Material redMaterial;
+
+    public bool activated = false; //true = bola esta no labirinto / false = bola esta fora do labirinto
+    private bool canUse = false; //true = boy esta por baixo do helmet / false = boy nao esta debaixo do helmet
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Boy") && activated)
+        {
+            canUse = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.Equals("Boy") && activated)
+        {
+            canUse = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && canUse)
+        {
+            controlBall();
+        }
+    }
+
+    public void activate() {
+        activated = activated ? false : true;
+        myRenderer.material = activated ? greenMaterial : redMaterial;
+    }
+
+    void controlBall() { 
+    }
+}
