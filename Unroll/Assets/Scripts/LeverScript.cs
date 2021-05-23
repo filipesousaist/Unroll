cb7ef3ec5871b3a3a6fe256e7df7751908aae5b3
j-galveias <brainsuprajr@gmail.com>
2021-05-24 00:19:47 +0100

    easy-3 and plinko prefab

---
+++ Unroll/Assets/Scripts/LeverScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    public Transform plank;
+    public Boy boy;
+    bool canInteract = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canInteract = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canInteract = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canInteract || boy.HasBall())
+        {
+            canInteract = false;
+        }
+
+        if (canInteract)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                plank.Rotate(0, 0, 10);
+            }
+        }
+    }
+}
