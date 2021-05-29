e423cc52a2605c44010cae740b7797275699e52e
ist190714 <ist190714@tecnico.ulisboa.pt>
2021-05-29 02:49:08 +0100

    stash_apply

---
+++ Unroll/Assets/Scripts/Const.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public static class Const
+{
+    public static readonly string[] LEVEL_NAMES = new string[]
+    {
+        "Easy-1",
+        "Easy-2",
+        "Easy-3",
+        "Intermediate-1"
+    };
+
+    public static readonly Collectible.Metal[] COLLECTIBLE_METALS = new Collectible.Metal[]{
+        Collectible.Metal.Copper,
+        Collectible.Metal.Silver,
+        Collectible.Metal.Gold
+    };
+}
