f6aa98f31c2680a3e03d9fff22df6afd5bd22d02
pedro19v <ist190762@tecnico.ulisboa.pt>
2021-05-29 19:53:00 +0100

    Started to implement some of the analytics

--- Unroll/Assets/Scripts/Characters/Ball.cs
+++ Unroll/Assets/Scripts/Characters/Ball.cs
@@ -60,7 +60,10 @@ public class Ball : MonoBehaviour
 
     private void Update()
     {
-        ballColorTextBox.text = color.color;
+        if (ballColorTextBox != null)
+        {
+            ballColorTextBox.text = color.color;
+        }
 
         if (boy.HasBall())
         {
